980c3b7703ae5d09f150f2c27b40de0e3b4ed716
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-13 18:09:44 -0300

    feat: finalizando layout dashboard-page

--- src/app/components/ButtonAddTask.tsx
+++ src/app/components/ButtonAddTask.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ButtonAddTask() {
   return (
-    <button class></button>
+    <button className="btn btn-primary bg-sky-950 text-zinc-50 border-none right-36 absolute">Adicionar Tarefa</button>
   )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Header from '../components/Header'
 import { signIn } from 'next-auth/react'
 import Tasks from '../components/tasks/Tasks'
+import ButtonAddTask from '../components/ButtonAddTask'
 
 export default function Page() {
   return (
     <main className='bg-zinc-100 h-screen'>
       <Header/>
-      <h1 className='mt-8 font-bold mx-28 text-2xl  text-sky-900'>Olá, {signIn.name}</h1>
-      <h1 className='font-bold mx-28 text-lg text-sky-900'>Aqui estão as suas tarefas</h1>
-      
+      <div className="flex items-center relative">
+        <div className="">
+          <h1 className='mt-8 font-bold mx-28 text-2xl  text-sky-900'>Olá, {signIn.name}</h1>
+          <h1 className='font-bold mx-28 text-lg text-sky-900'>Aqui estão as suas tarefas</h1>
+        </div>
+        <ButtonAddTask/>
+      </div>
       <Tasks/>
 
     </main>
